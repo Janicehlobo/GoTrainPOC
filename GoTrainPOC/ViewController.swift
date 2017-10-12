384f7f66b621c58b0e3d30c0138bfcdb536f0211
Bahram Haddadi <bahram@ca.ibm.com>
2017-10-12 09:32:26 -0300

    Initial commit

---
+++ GoTrainPOC/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GoTrainPOC
+//
+//  Created by Bahram Haddadi on 2017-10-12.
+//  Copyright © 2017 Bahram Haddadi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
